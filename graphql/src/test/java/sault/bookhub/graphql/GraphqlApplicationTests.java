101da320229a7ca63a7336d25ecea1d91f9de890
Angkit Khadka <khadkaankit85@gmail.com>
2025-01-09 09:41:26 -0500

    started graphql with spring boot

---
+++ graphql/src/test/java/sault/bookhub/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package sault.bookhub.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
